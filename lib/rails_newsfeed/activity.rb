8bf85a81fc82a03dfcbfb0bea8a78c7a795cc7dc
Nguyen Tri Thanh <thanhnt1@greenglobal.vn>
2016-03-24 23:15:34 +0700

    Change test

--- lib/rails_newsfeed/activity.rb
+++ lib/rails_newsfeed/activity.rb
@@ -19,72 +19,61 @@ module RailsNewsfeed
     # gets schema
     # DO NOT override this method unless you know what you are doing
     def self.schema
-      { id: :uuid, content: :text, time: :timestamp, object: :text }
+      { id: :uuid, content: :text, object: :text, time: :timestamp }
+    end
+
+    # gets all activities
+    def self.all(opt = {})
+      rs = Connection.select(table_name, schema, '*') if opt.empty?
+      rs = Connection.select(index_table_name, schema, '*', object: opt[:object]) if opt.key?(:object)
+      acts = []
+      rs.each { |r| acts.push(create_from_cass(:act, r)) } if rs
+      acts
+    end
+
+    # creates activity
+    def self.create(opt = {})
+      act = new(opt)
+      return act if act.save
+      nil
     end
 
     # finds activity by id
     def self.find(id)
       r = Connection.select(table_name, schema, '*', { id: id }, page_size: 1).first
       return nil unless r
-      create_from_cass_act(r)
+      create_from_cass(:act, r)
     end
 
-    # hides all feeds of object
-    def self.hide_all_of(object)
-      return true unless object
-      Connection.select(index_table_name, schema, '*', object: object).each do |r|
-        id = r['id'].to_s
-        Connection.delete(table_name, schema, id: id)
-        delete_from_feed(id)
+    # deletes activities
+    def self.delete(opt = {}, show_last = true)
+      if opt.key?(:id)
+        act = find(opt[:id])
+        return true unless act
+        return act.delete(show_last)
+      elsif opt.key?(:object)
+        all(object: opt[:object]).each { |a| a.delete(false) }
+        return true
+      else
+        # truncates activity table and feed tables
+        Connection.exec_cql("TRUNCATE #{table_name}")
+        Connection.exec_cql("TRUNCATE #{index_table_name}")
+        FeedTable.all.each { |r| Connection.exec_cql("TRUNCATE #{r.class.table_name}") }
+        return true
       end
-      true
-    end
-
-    # deletes from all feed tables
-    def self.delete_from_feed(id, last = nil)
-      cqls = []
-      FeedTable.all.each do |i|
-        i_tbl = i.class.table_name
-        i_schema = i.class.schema
-        Connection.select(i_tbl, i_schema, '*', { activity_id: id }, filtering: true).each do |r|
-          cqls.push(Connection.delete(i_tbl, i_schema, { id: r['id'], activity_id: r['activity_id'].to_s }, true))
-          next unless last
-          cqls.push(Connection.insert(i_tbl, i_schema, NewsfeedModel.from_cass_act(r['id'], last), true))
-        end
-      end
-      Connection.batch_cqls(cqls.uniq) unless cqls.empty?
-      true
-    end
-
-    # deletes an activity by id including activities from feed tables
-    def self.delete(id, show_last = true)
-      act = find(id)
-      return true unless act
-      act.delete(show_last)
     end
 
     # creates from feed cassandra
-    def self.create_from_cass_feed(res)
-      new(from_cass(:feed, res))
-    end
-
-    # creates from activity cassandra
-    def self.create_from_cass_act(res)
-      new(from_cass(:act, res))
-    end
-
-    # gets hash from cassandra
-    def self.from_cass(type, res)
+    def self.create_from_cass(type, res)
       h = {}
       temp = type == :act ? '' : 'activity_'
       schema.keys.each do |k|
         key = "#{temp}#{k}"
-        val = k == :id ? res[key].to_s : res[key]
-        val = nil if val == ''
+        val = k == :id || k == :time ? res[key].to_s : res[key]
         h[k] = val
       end
       h[:new_record] = false
-      h
+      new(h)
     end
 
     # initializes
@@ -105,26 +94,25 @@ module RailsNewsfeed
     # deletes including activities from feed tables
     def delete(show_last = true)
       return false if @new_record
-      return false unless Connection.delete(self.class.table_name, self.class.schema, id: @id)
-      l = nil
-      if @object
-        Connection.delete(self.class.index_table_name, self.class.schema, object: @object, id: @id)
-        l = Connection.select(self.class.index_table_name, self.class.schema, '*', object: @object).first if show_last
-      end
-      self.class.delete_from_feed(@id, l)
+      Connection.delete(self.class.table_name, self.class.schema, id: @id)
+      Connection.delete(self.class.index_table_name, self.class.schema, object: @object, id: @id) if @object
+      return delete_from_feed(@id, nil) unless show_last
+      delete_from_feed(@id, last)
     end
 
     # converts to hash
-    def to_h
-      { id: @id, content: @content, time: @time, object: @object }
+    def to_h(prefix = nil)
+      { "#{prefix}id".to_sym => @id, "#{prefix}content".to_sym => @content,
+        "#{prefix}object".to_sym => @object, "#{prefix}time".to_sym => @time }
     end
 
     protected
 
       # inserts
       def insert
+        return false unless @content
         @id ||= Cassandra::Uuid::Generator.new.now.to_s
-        @time ||= DateTime.current.strftime('%Y-%m-%d %H:%M:%S%z')
+        @time ||= Cassandra::Types::Timestamp.new(DateTime.current).to_s
         Connection.insert(self.class.table_name, self.class.schema, to_h)
         Connection.insert(self.class.index_table_name, self.class.schema, to_h) unless @object.nil?
         @new_record = false
@@ -134,6 +122,39 @@ module RailsNewsfeed
       # updates
       def update
         Connection.update(self.class.table_name, self.class.schema, { id: @id }, to_h)
+        # updates from all feed models
+        cqls = []
+        FeedTable.all.each do |i|
+          i_tbl = i.class.table_name
+          i_schema = i.class.schema
+          Connection.select(i_tbl, i_schema, '*', { activity_id: @id }, filtering: true).each do |r|
+            cqls.push(Connection.update(i_tbl, i_schema, { id: r['id'], activity_id: @id }, to_h('activity_'), true))
+          end
+        end
+        Connection.batch_cqls(cqls)
+        true
+      end
+
+      # deletes from all feed tables
+      def delete_from_feed(id, last = nil)
+        cqls = []
+        FeedTable.all.each do |i|
+          i_tbl = i.class.table_name
+          i_schema = i.class.schema
+          Connection.select(i_tbl, i_schema, '*', { activity_id: id }, filtering: true).each do |r|
+            cqls.push(Connection.delete(i_tbl, i_schema, { id: r['id'], activity_id: r['activity_id'].to_s }, true))
+            next unless last
+            cqls.push(Connection.insert(i_tbl, i_schema, NewsfeedModel.from_cass_act(r['id'], last), true))
+          end
+        end
+        Connection.batch_cqls(cqls.uniq) unless cqls.empty?
+        true
+      end
+
+      # gets last activity of object
+      def last
+        return nil unless @object
+        Connection.select(self.class.index_table_name, self.class.schema, '*', object: @object).first
       end
   end
 end
--- spec/activity_spec.rb
+++ spec/activity_spec.rb
@@ -1,97 +1,322 @@
-RSpec.describe 'Activity' do
-  tbl = RailsNewsfeed::Activity.table_name
-  index_tbl = RailsNewsfeed::Activity.index_table_name
-
-  describe 'Get non-existed activity' do
-    it 'OK' do
-      # truncates first
-      RailsNewsfeed::Connection.exec_cql("TRUNCATE #{tbl}")
-      RailsNewsfeed::Connection.exec_cql("TRUNCATE #{index_tbl}")
-      act = RailsNewsfeed::Activity.find(SecureRandom.uuid)
-      expect(act).to eq(nil)
-    end
+RSpec.describe RailsNewsfeed::Activity do
+  describe 'initials new instance with empty arguments' do
+    get_act = RailsNewsfeed::Activity.new
+    subject { get_act }
+    it { is_expected.to have_attributes(id: nil) }
+    it { is_expected.to have_attributes(content: nil) }
+    it { is_expected.to have_attributes(object: nil) }
+    it { is_expected.to have_attributes(time: nil) }
+    it { is_expected.to have_attributes(new_record: true) }
   end
 
-  describe 'Add a new activity' do
-    it 'without object' do
-      # truncates first
-      RailsNewsfeed::Connection.exec_cql("TRUNCATE #{tbl}")
-      RailsNewsfeed::Connection.exec_cql("TRUNCATE #{index_tbl}")
-      activity_content = 'user 1 post photo 1'
-      activity = RailsNewsfeed::Activity.new(content: activity_content)
-      result = activity.save
-      expect(result).to eq(true)
-      act = RailsNewsfeed::Activity.find(activity.id)
-      expect(act).not_to eq(nil)
-      expect(act.id.to_s).to eq(activity.id)
-      expect(act.content).to eq(activity.content)
-      expect(act.object).to eq(activity.object)
-      expect(act.time).to eq(activity.time)
-      # truncates when finish
-      RailsNewsfeed::Connection.exec_cql("TRUNCATE #{tbl}")
-      RailsNewsfeed::Connection.exec_cql("TRUNCATE #{index_tbl}")
-    end
-
-    it 'with object' do
-      # truncates first
-      RailsNewsfeed::Connection.exec_cql("TRUNCATE #{tbl}")
-      RailsNewsfeed::Connection.exec_cql("TRUNCATE #{index_tbl}")
-      activity = RailsNewsfeed::Activity.new(content: 'user 1 post photo 1', object: SecureRandom.hex)
-      result = activity.save
-      expect(result).to eq(true)
-      act = RailsNewsfeed::Activity.find(activity.id)
-      expect(act).not_to eq(nil)
-      expect(act.id).to eq(activity.id)
-      expect(act.content).to eq(activity.content)
-      expect(act.object).to eq(activity.object)
-      expect(act.time).to eq(activity.time)
-      # truncates when finish
-      RailsNewsfeed::Connection.exec_cql("TRUNCATE #{tbl}")
-      RailsNewsfeed::Connection.exec_cql("TRUNCATE #{index_tbl}")
-    end
+  describe 'initials new instance with arguments' do
+    args = { id: SecureRandom.uuid, content: 'user 1 post photo 1', object: 'photo 1',
+             time: Cassandra::Types::Timestamp.new(DateTime.current).to_s, new_record: false }
+    get_act = RailsNewsfeed::Activity.new(args)
+    subject { get_act }
+    it { is_expected.to have_attributes(id: args[:id]) }
+    it { is_expected.to have_attributes(content: args[:content]) }
+    it { is_expected.to have_attributes(object: args[:object]) }
+    it { is_expected.to have_attributes(time: args[:time]) }
+    it { is_expected.to have_attributes(new_record: false) }
   end
 
-  describe 'Delete an activity' do
-    it 'without object' do
-      # truncates first
-      RailsNewsfeed::Connection.exec_cql("TRUNCATE #{tbl}")
-      RailsNewsfeed::Connection.exec_cql("TRUNCATE #{index_tbl}")
-      activity = RailsNewsfeed::Activity.new(content: 'user 1 post photo 1')
-      activity.save
-      act = RailsNewsfeed::Activity.find(activity.id)
-      expect(act).not_to eq(nil)
-      expect(act.id.to_s).to eq(activity.id)
-      expect(act.content).to eq(activity.content)
-      expect(act.object).to eq(activity.object)
-      expect(act.time).to eq(activity.time)
-      result = activity.delete
-      expect(result).to eq(true)
-      act = RailsNewsfeed::Activity.find(activity.id)
-      expect(act).to eq(nil)
-      # truncates when finish
-      RailsNewsfeed::Connection.exec_cql("TRUNCATE #{tbl}")
-      RailsNewsfeed::Connection.exec_cql("TRUNCATE #{index_tbl}")
-    end
+  describe 'converts to hash without prefix' do
+    # truncates first
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.table_name}")
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.index_table_name}")
+    args = { id: SecureRandom.uuid, content: 'user 1 post photo 1', object: 'photo 1',
+             time: Cassandra::Types::Timestamp.new(DateTime.current).to_s, new_record: false }
+    hash = RailsNewsfeed::Activity.new(args).to_h
+    subject { hash }
+    it { is_expected.to be_a(Hash) }
+    it { is_expected.not_to be_empty }
+    it { is_expected.to include(id: args[:id]) }
+    it { is_expected.to include(content: args[:content]) }
+    it { is_expected.to include(object: args[:object]) }
+    it { is_expected.to include(time: args[:time]) }
+  end
 
-    it 'with object' do
-      # truncates first
-      RailsNewsfeed::Connection.exec_cql("TRUNCATE #{tbl}")
-      RailsNewsfeed::Connection.exec_cql("TRUNCATE #{index_tbl}")
-      activity = RailsNewsfeed::Activity.new(content: 'user 1 post photo 1', object: SecureRandom.hex)
-      activity.save
-      act = RailsNewsfeed::Activity.find(activity.id)
-      expect(act).not_to eq(nil)
-      expect(act.id).to eq(activity.id)
-      expect(act.content).to eq(activity.content)
-      expect(act.object).to eq(activity.object)
-      expect(act.time).to eq(activity.time)
-      result = activity.delete
-      expect(result).to eq(true)
-      act = RailsNewsfeed::Activity.find(activity.id)
-      expect(act).to eq(nil)
-      # truncates when finish
-      RailsNewsfeed::Connection.exec_cql("TRUNCATE #{tbl}")
-      RailsNewsfeed::Connection.exec_cql("TRUNCATE #{index_tbl}")
-    end
+  describe 'converts to hash with prefix' do
+    # truncates first
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.table_name}")
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.index_table_name}")
+    args = { id: SecureRandom.uuid, content: 'user 1 post photo 1', object: 'photo 1',
+             time: Cassandra::Types::Timestamp.new(DateTime.current).to_s, new_record: false }
+    prefix = 'activity_'
+    hash = RailsNewsfeed::Activity.new(args).to_h(prefix)
+    subject { hash }
+    it { is_expected.to be_a(Hash) }
+    it { is_expected.not_to be_empty }
+    it { is_expected.to include("#{prefix}id".to_sym => args[:id]) }
+    it { is_expected.to include("#{prefix}content".to_sym => args[:content]) }
+    it { is_expected.to include("#{prefix}object".to_sym => args[:object]) }
+    it { is_expected.to include("#{prefix}time".to_sym => args[:time]) }
+  end
+
+  describe 'gets table name' do
+    subject { RailsNewsfeed::Activity.table_name }
+    it { is_expected.to be_a(String) }
+    it { is_expected.not_to be_empty }
+  end
+
+  describe 'gets index table name' do
+    subject { RailsNewsfeed::Activity.index_table_name }
+    it { is_expected.to be_a(String) }
+    it { is_expected.not_to be_empty }
+  end
+
+  describe 'gets schema' do
+    subject { RailsNewsfeed::Activity.schema }
+    it { is_expected.to be_a(Hash) }
+    it { is_expected.not_to be_empty }
+    it { is_expected.to include(:id) }
+    it { is_expected.to include(:content) }
+    it { is_expected.to include(:object) }
+    it { is_expected.to include(:time) }
+  end
+
+  describe 'inserts a new activity without content' do
+    # truncates first
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.table_name}")
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.index_table_name}")
+    act = RailsNewsfeed::Activity.new
+    saved = act.save
+    subject { saved }
+    it { is_expected.to eq(false) }
+  end
+
+  describe 'inserts a new activity without content (self)' do
+    # truncates first
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.table_name}")
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.index_table_name}")
+    act = RailsNewsfeed::Activity.create
+    subject { act }
+    it { is_expected.to eq(nil) }
+  end
+
+  describe 'inserts a new activity with content' do
+    # truncates first
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.table_name}")
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.index_table_name}")
+    act = RailsNewsfeed::Activity.new(content: 'user 1 post photo 1')
+    saved = act.save
+    subject { saved }
+    it { is_expected.to eq(true) }
+  end
+
+  describe 'inserts a new activity with content (self)' do
+    # truncates first
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.table_name}")
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.index_table_name}")
+    act = RailsNewsfeed::Activity.create(content: 'user 1 post photo 1')
+    subject { act }
+    it { is_expected.not_to eq(nil) }
+    it { is_expected.to be_a(RailsNewsfeed::Activity) }
+  end
+
+  describe 'parses from cassandra result (act)' do
+    # truncates first
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.table_name}")
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.index_table_name}")
+    args = { content: 'user 1 post photo 1', object: 'photo 1' }
+    act = RailsNewsfeed::Activity.new(args)
+    act.save
+    res = RailsNewsfeed::Connection.select(RailsNewsfeed::Activity.table_name, RailsNewsfeed::Activity.schema,
+                                           '*', id: act.id).first
+    get_act = RailsNewsfeed::Activity.create_from_cass(:act, res)
+    subject { get_act }
+    it { is_expected.not_to eq(nil) }
+    it { is_expected.to be_a(RailsNewsfeed::Activity) }
+    it { is_expected.to have_attributes(id: act.id) }
+    it { is_expected.to have_attributes(content: act.content) }
+    it { is_expected.to have_attributes(object: act.object) }
+    it { is_expected.to have_attributes(time: act.time) }
+    it { is_expected.to have_attributes(new_record: false) }
+  end
+
+  describe 'finds non-existed activity' do
+    # truncates first
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.table_name}")
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.index_table_name}")
+    get_act = RailsNewsfeed::Activity.find(SecureRandom.uuid)
+    subject { get_act }
+    it { is_expected.to eq(nil) }
+  end
+
+  describe 'finds existed activity' do
+    # truncates first
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.table_name}")
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.index_table_name}")
+    args = { content: 'user 1 post photo 1', object: 'photo 1' }
+    act = RailsNewsfeed::Activity.new(args)
+    act.save
+    get_act = RailsNewsfeed::Activity.find(act.id)
+    subject { get_act }
+    it { is_expected.not_to eq(nil) }
+    it { is_expected.to be_a(RailsNewsfeed::Activity) }
+    it { is_expected.to have_attributes(id: act.id) }
+    it { is_expected.to have_attributes(content: act.content) }
+    it { is_expected.to have_attributes(object: act.object) }
+    it { is_expected.to have_attributes(time: act.time) }
+    it { is_expected.to have_attributes(new_record: false) }
+  end
+
+  describe 'gets all activities' do
+    # truncates first
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.table_name}")
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.index_table_name}")
+    normal = Random.rand(1_000) + 1
+    acts = []
+    (1..normal).each { |i| acts.push(RailsNewsfeed::Activity.create(content: "activity #{i}")) }
+    get_acts = RailsNewsfeed::Activity.all
+    subject { get_acts }
+    it { is_expected.to be_a(Array) }
+    it { is_expected.to all(be_a(RailsNewsfeed::Activity)) }
+    it { is_expected.to have_exactly(normal).items }
+  end
+
+  describe 'gets all activities of object' do
+    # truncates first
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.table_name}")
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.index_table_name}")
+    normal = Random.rand(1_000) + 1
+    total_object = Random.rand(1_000) + 1
+    object = 'test'
+    acts = []
+    (1..normal).each { |i| acts.push(RailsNewsfeed::Activity.create(content: "activity #{i}")) }
+    (1..total_object).each { |i| acts.push(RailsNewsfeed::Activity.create(content: "activity #{i}", object: object)) }
+    get_acts = RailsNewsfeed::Activity.all(object: object)
+    subject { get_acts }
+    it { is_expected.to be_a(Array) }
+    it { is_expected.to all(be_a(RailsNewsfeed::Activity)) }
+    it { is_expected.to have_exactly(total_object).items }
+  end
+
+  describe 'deletes activity' do
+    # truncates first
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.table_name}")
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.index_table_name}")
+    args = { content: 'user 1 post photo 1', object: 'photo 1' }
+    act = RailsNewsfeed::Activity.new(args)
+    act.save
+    deleted = act.delete
+    subject { deleted }
+    it { is_expected.to eq(true) }
+  end
+
+  describe 'deletes activity (logic)' do
+    # truncates first
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.table_name}")
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.index_table_name}")
+    args = { content: 'user 1 post photo 1', object: 'photo 1' }
+    act = RailsNewsfeed::Activity.new(args)
+    act.save
+    act.delete
+    get_acts = RailsNewsfeed::Activity.all
+    subject { get_acts }
+    it { is_expected.to be_a(Array) }
+    it { is_expected.to be_empty }
+  end
+
+  describe 'deletes activity (self)' do
+    # truncates first
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.table_name}")
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.index_table_name}")
+    args = { content: 'user 1 post photo 1', object: 'photo 1' }
+    act = RailsNewsfeed::Activity.new(args)
+    act.save
+    deleted = RailsNewsfeed::Activity.delete(id: act.id)
+    subject { deleted }
+    it { is_expected.to eq(true) }
+  end
+
+  describe 'deletes activity (self) (logic)' do
+    # truncates first
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.table_name}")
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.index_table_name}")
+    args = { content: 'user 1 post photo 1', object: 'photo 1' }
+    act = RailsNewsfeed::Activity.new(args)
+    act.save
+    RailsNewsfeed::Activity.delete(id: act.id)
+    get_acts = RailsNewsfeed::Activity.all
+    subject { get_acts }
+    it { is_expected.to be_a(Array) }
+    it { is_expected.to be_empty }
+  end
+
+  describe 'deletes all activities' do
+    # truncates first
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.table_name}")
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.index_table_name}")
+    normal = Random.rand(1_000) + 1
+    acts = []
+    (1..normal).each { |i| acts.push(RailsNewsfeed::Activity.create(content: "activity #{i}")) }
+    deleted = RailsNewsfeed::Activity.delete
+    subject { deleted }
+    it { is_expected.to eq(true) }
+  end
+
+  describe 'deletes all activities (logic)' do
+    # truncates first
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.table_name}")
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.index_table_name}")
+    normal = Random.rand(1_000) + 1
+    acts = []
+    (1..normal).each { |i| acts.push(RailsNewsfeed::Activity.create(content: "activity #{i}")) }
+    RailsNewsfeed::Activity.delete
+    get_acts = RailsNewsfeed::Activity.all
+    subject { get_acts }
+    it { is_expected.to be_a(Array) }
+    it { is_expected.to be_empty }
+  end
+
+  describe 'deletes all activities of object' do
+    # truncates first
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.table_name}")
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.index_table_name}")
+    normal = Random.rand(1_000) + 1
+    total_object = Random.rand(1_000) + 1
+    object = 'test'
+    acts = []
+    (1..normal).each { |i| acts.push(RailsNewsfeed::Activity.create(content: "activity #{i}")) }
+    (1..total_object).each { |i| acts.push(RailsNewsfeed::Activity.create(content: "activity #{i}", object: object)) }
+    deleted = RailsNewsfeed::Activity.delete(object: object)
+    subject { deleted }
+    it { is_expected.to eq(true) }
+  end
+
+  describe 'deletes all activities of object (logic) (object)' do
+    # truncates first
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.table_name}")
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.index_table_name}")
+    normal = Random.rand(1_000) + 1
+    total_object = Random.rand(1_000) + 1
+    object = 'test'
+    acts = []
+    (1..normal).each { |i| acts.push(RailsNewsfeed::Activity.create(content: "activity #{i}")) }
+    (1..total_object).each { |i| acts.push(RailsNewsfeed::Activity.create(content: "activity #{i}", object: object)) }
+    RailsNewsfeed::Activity.delete(object: object)
+    get_acts = RailsNewsfeed::Activity.all(object: object)
+    subject { get_acts }
+    it { is_expected.to be_a(Array) }
+    it { is_expected.to be_empty }
+  end
+
+  describe 'deletes all activities of object (logic) (all)' do
+    # truncates first
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.table_name}")
+    RailsNewsfeed::Connection.exec_cql("TRUNCATE #{RailsNewsfeed::Activity.index_table_name}")
+    normal = Random.rand(1_000) + 1
+    total_object = Random.rand(1_000) + 1
+    object = 'test'
+    acts = []
+    (1..normal).each { |i| acts.push(RailsNewsfeed::Activity.create(content: "activity #{i}")) }
+    (1..total_object).each { |i| acts.push(RailsNewsfeed::Activity.create(content: "activity #{i}", object: object)) }
+    RailsNewsfeed::Activity.delete(object: object)
+    get_acts = RailsNewsfeed::Activity.all
+    subject { get_acts }
+    it { is_expected.to be_a(Array) }
+    it { is_expected.to have_exactly(normal).items }
   end
 end
